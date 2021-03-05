e6cee8a6073f75f81843b1d5149d9e3d44f86e4f
M0Nasser <nasseruser12@gmail.com>
2021-03-05 18:18:30 +0200

    add ui elemnts

---
+++ Assets/Scripts/Mono Scrpits/UiManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playerUi;
+    [SerializeField]
+    GameObject abilityParentObj;
+    [SerializeField]
+    GameObject abilityObj;
+    [SerializeField]
+    GameObject gameOverObj;
+    [SerializeField]
+    GameObject pauseGameObj;
+    [SerializeField]
+    GameObject victoryGameObj;
+    [SerializeField]
+    PlayerUiElemnt PlayerUiElemnt;
+
+    float playerAbilityCount;
+
+
+    private void Awake()
+    {
+        playerUi.transform.GetChild(0).GetComponent<Image>().sprite = PlayerUiElemnt.PlayerIcon;
+        playerAbilityCount = PlayerUiElemnt.PlayerAbilities.Length;
+        for (int i = 0; i < playerAbilityCount; i++)
+        {
+
+            GameObject childObject = Instantiate(abilityObj) as GameObject;
+            childObject.transform.parent = abilityParentObj.transform;
+            childObject.transform.GetChild(0).GetComponent<Image>().sprite = PlayerUiElemnt.PlayerAbilities[i].AbilityIcon;
+            childObject.transform.GetChild(1).GetComponent<Image>().sprite = PlayerUiElemnt.PlayerAbilities[i].AbilityValueIcon;
+            childObject.transform.GetChild(2).GetComponent<TMPro.TMP_Text>().text = PlayerUiElemnt.PlayerAbilities[i].AbilityValue.ToString();
+
+
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverObj.SetActive(false);
+        victoryGameObj.SetActive(false);
+        pauseGameObj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnUiPuasedPannelCalled();
+            Debug.Log("OnUiPuasedPannelCalled");
+        }
+    }
+    public void OnUiButtonExit()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+    public void OnUiButtonRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void OnUiButtonNextLvel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void OnUiButtonResume()
+    {
+       // Time.timeScale = 1;
+        Debug.Log("called ");
+        pauseGameObj.SetActive(false);
+    }
+
+    public void OnUiVictoryPannelCalled()
+    {
+        victoryGameObj.SetActive(true);
+    }
+
+    public void OnUiGameOverPannelCalled()
+    {
+        gameOverObj.SetActive(true);
+    }
+
+    public void OnUiPuasedPannelCalled()
+    {
+        //Time.timeScale = 0;
+        pauseGameObj.SetActive(true);
+        
+    }
+
+}
---
+++ Assets/Scripts/SO scripts/PlayerUiElemnt.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class PlayerUiElemnt : ScriptableObject
+{
+    public Sprite PlayerIcon;
+    public PlayerAbility[] PlayerAbilities; 
+}
